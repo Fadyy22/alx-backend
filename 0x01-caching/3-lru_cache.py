231f7df16eb0dc1400a8abafcc1485e2dc327ad2
Fadyy22 <fadyalaa441@gmail.com>
2024-01-31 16:52:17 +0200

    create lru caching class

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -2,17 +2,11 @@
 
 """module containing LRUCache class"""
 
-from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
     """LRUCache class that uses LRU caching system"""
-
-    def __init__(self):
-        """constructor function"""
-        super().__init__()
-        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -20,14 +14,16 @@ class LRUCache(BaseCaching):
         if key is not None and item is not None:
             self.cache_data[key] = item
             if len(list(self.cache_data.keys())) > self.MAX_ITEMS:
-                least_recently_used_key = self.cache_data.popitem(False)
-                print(f"DISCARD: {least_recently_used_key[0]}")
+                least_recenyl_used_key = list(self.cache_data.keys())[0]
+                del self.cache_data[least_recenyl_used_key]
+                print(f"DISCARD: {least_recenyl_used_key}")
 
     def get(self, key):
         """ Get an item by key
         """
         if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, True)
+            value = self.cache_data.pop(key)
+            self.cache_data[key] = value
             return self.cache_data[key]
         else:
             return None
