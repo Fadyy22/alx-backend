0c91297e86bdef6c4cbf8c900bcdaabd0cea1a9b
Fadyy22 <fadyalaa441@gmail.com>
2024-01-31 15:24:55 +0200

    create FIFO caching class

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -12,11 +12,14 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """puts a cache item in cache_data attribute using FIFO policy"""
         if key is not None and item is not None:
-            self.cache_data[key] = item
-            if len(list(self.cache_data.keys())) > self.MAX_ITEMS:
-                first_key = list(self.cache_data.keys())[0]
-                del self.cache_data[first_key]
-                print(f"DISCARD: {first_key}")
+            if len(list(self.cache_data.keys())) == self.MAX_ITEMS:
+                if key not in list(self.cache_data.keys()):
+                    first_key = list(self.cache_data.keys())[0]
+                    del self.cache_data[first_key]
+                    print(f"DISCARD: {first_key}")
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
 
     def get(self, key):
         """gets a cache item from cache_data by key"""
