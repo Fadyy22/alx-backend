cbd2b8f2813f4a78ce08e5ea24ebb0e40ac0fa0a
Fadyy22 <fadyalaa441@gmail.com>
2024-02-06 18:17:19 +0200

    basic flask app that renders simple html page

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""basic Flask app with single route"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """render 0-index.html page"""
+    return render_template('0-index.html')
+
+
+app.run()
