17b93c29b98fa9f65258fc40009fc3cfcb364966
Fadyy22 <fadyalaa441@gmail.com>
2024-03-05 19:33:52 +0200

    get and set in redis

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,7 +3,7 @@ import { createClient } from 'redis';
 const client = createClient();
 
 client.on('error', (error) => {
-  console.log(`Redis client not connected to the server: ${error}`);
+  console.log(`Redis client not connected to the server: ${error.toString()}`);
 });
 
 client.on('connect', () => {
