5d0160bcbc1ad4a1ae5c550ae422601282dd60cc
Fadyy22 <fadyalaa441@gmail.com>
2024-03-06 21:58:34 +0200

    create push notificaton job function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,25 @@
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!(jobs instanceof Array)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  for (const jobData of jobs) {
+    const job = queue.create('push_notification_code_3', jobData);
+    job
+      .on('enqueue', () => {
+        console.log(`Notification job created: ${job.id}`);
+      })
+      .on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      })
+      .on('failed', (err) => {
+        console.log(`Notification job ${job.id} failed: ${err.toString()}`);
+      })
+      .on('progress', (progress) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+    job.save();
+  }
+};
+
+export default createPushNotificationsJobs;
